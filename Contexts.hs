80d6f29fc8849cade3b9295a100d086947b54010
Simonas Kazlauskas <git@kazlauskas.me>
2013-04-17 21:13:16 +0300

    Implement copyright metadata
    Because I can. Actually I just wanted to check how much pain in ass
    would it be to implement it. Surprisingly it wasn't so bad, but it also
    could be better.
    
    Also responsive footer content.

--- Contexts.hs
+++ Contexts.hs
@@ -5,9 +5,11 @@ module Contexts
     , baseContext
     ) where
 
-import Hakyll
-import Data.Monoid            (mappend, mconcat)
-import Text.Pandoc.Options
+import           Control.Applicative    ((<|>), empty)
+import qualified Data.Map as M
+import           Data.Monoid            (mappend, mconcat)
+import           Hakyll
+import           Text.Pandoc.Options
 
 
 entryContext tags = mconcat [ dateField "date" "%Y-%m-%d"
@@ -24,7 +26,12 @@ feedContext = bodyField "description" `mappend` defaultContext
 indexContext entries = constField "entries" entries `mappend` defaultContext
 
 
-baseContext = defaultContext
+baseContext = copyField
+              `mappend` copyURLField "copy-url" "copy" copyField
+              `mappend` copyLongField "copy-long" "copy" copyField
+              `mappend` defaultContext
+  where
+    copyField = defaultField "copy" "CC BY 3.0"
 
 
 tocField :: String -> Context a
@@ -45,3 +52,25 @@ notEmptyField (Context a) = Context $ \k i -> do
         []        -> fail $ "Empty field "++ k ++" in context for item "
                             ++ show (itemIdentifier i)
         _ -> return value
+
+
+defaultField :: String -> String -> Context String
+defaultField key defval = Context $ \k i ->
+    if key == k then unContext metadataField k i <|> return defval else empty
+
+
+copyURLField, copyLongField :: String -> String -> Context a -> Context a
+copyURLField  = copyGenField snd
+copyLongField = copyGenField fst
+copyGenField func key from (Context a) = Context $ \k i ->
+    if key == k
+    then a from i >>= \lc -> return $ func $ licences M.! lc
+    else empty
+  where
+    licences = M.fromList
+        [ ("CC BY 3.0", ( "Creative Commons Attribution 3.0 Unported"
+          , "http://creativecommons.org/licenses/by/3.0/deed.en_GB"))
+        , ("WTFPL", ("Do What the F*** You Want to Public License"
+          , "http://www.wtfpl.net/about/"))
+        -- Add on demand
+        ]
