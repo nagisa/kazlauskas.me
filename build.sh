27e19fd045ffce7125fe6e6f23d35bbf4d5e37cb
Simonas Kazlauskas <git@kazlauskas.me>
2021-11-22 00:43:14 +0200

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+workdir=$(mktemp -d)
+trap 'rm -rf "$workdir"' EXIT
+
+curl_retries=5
+while ! curl -o "$workdir/install" -v --fail -L "${INPUT_INSTALL_URL:-https://nixos.org/nix/install}"
+do
+  sleep 1
+  ((curl_retries--))
+  if [[ $curl_retries -le 0 ]]; then
+    echo "curl retries failed" >&2
+    exit 1
+  fi
+done
+
+sh "$workdir/install" --no-channel-add
+
+nix-build
