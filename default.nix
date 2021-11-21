b8ae314c6a2a5fbc78f4d77c8116f2cc7ffbcdf6
Simonas Kazlauskas <git@kazlauskas.me>
2021-11-22 00:30:20 +0200

    Test CF Pages…

--- default.nix
+++ default.nix
@@ -4,7 +4,9 @@ let
 lib = pkgs.lib;
 stdenv = pkgs.stdenv;
 
-site = stdenv.mkDerivation {
+in
+
+stdenv.mkDerivation {
   name = "www.kazlauskas.me";
   src = lib.cleanSourceWith {
     src = ./.;
@@ -17,35 +19,12 @@ site = stdenv.mkDerivation {
     );
   };
   buildInputs = [ (pkgs.haskellPackages.callPackage ./kazlauskas.nix {}) ];
-  outputs = ["images" "data" "out"];
+  outputs = ["out"];
   buildPhase = ''
     ls -alh
     kazlauskas build
   '';
   installPhase = ''
-    mkdir -p $images/var/www $data/var/www $out/var/
-    mv -v _site/images $images/var/www/images
-    mv -v _site/data $data/var/www/data
-    mv -v _site/ $out/var/www
+    mv -v _site/ $out
   '';
-};
-
-nginxConf = pkgs.runCommand "nginxConf" { preferLocalBuild = true; }
-  "mkdir $out && cp -vp ${./nginx.conf} $out/nginx.conf";
-
-nginxBase = pkgs.dockerTools.pullImage {
-  imageName = "nginx";
-  finalImageTag = "alpine";
-  imageDigest = "sha256:f51b557cbb5e8dfd8c5e416ae74b58fe823efe52d9f9fed3f229521844a509e2";
-  sha256 = "sha256-WBzZtRoBLwo5lUTwhMySMFMc4D4Y1tM2X43098G7FTQ=";
-};
-
-in pkgs.dockerTools.streamLayeredImage {
-  name = "kazlauskas-me";
-  tag = "latest";
-  fromImage = nginxBase;
-  contents = [nginxConf site.images site.data site.out];
-  config = {
-    Cmd = [ "nginx" "-c" "${nginxConf}/nginx.conf" ];
-  };
 }
