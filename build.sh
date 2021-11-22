30f96a535bc4461418b8945f76908507d437a319
Simonas Kazlauskas <git@kazlauskas.me>
2021-11-22 23:29:26 +0200

    Maybe try some actions action?

--- build.sh
+++ build.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env bash
 
-set -xe
-
-workdir=$(mktemp -d)
-trap 'rm -rf "$workdir"' EXIT
-
-curl_retries=5
-while ! curl -o "$workdir/install" -v --fail -L "${INPUT_INSTALL_URL:-https://nixos.org/nix/install}"
-do
-  sleep 1
-  ((curl_retries--))
-  if [[ $curl_retries -le 0 ]]; then
-    echo "curl retries failed" >&2
-    exit 1
-  fi
-done
-
-mkdir -m 0755 /nix && chown buildbot /nix
-sh "$workdir/install" --no-channel-add
-source $HOME/.nix-profile/etc/profile.d/nix.sh
-
-nix-build
+curl -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/nagisa/kazlauskas.me/actions/artifacts
